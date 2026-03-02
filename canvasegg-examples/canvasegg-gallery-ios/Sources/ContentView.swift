a26dd533d55771241b56e31e405fd4ff66fd369f
Hon-Atsugi-express <166384273+Hon-Atsugi-express@users.noreply.github.com>
2026-03-02 18:20:41 +0900

    Change

--- canvasegg-examples/canvasegg-gallery-ios/Sources/ContentView.swift
+++ canvasegg-examples/canvasegg-gallery-ios/Sources/ContentView.swift
@@ -4,7 +4,7 @@ import CanvasEggGalleryKit
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
+        MainKt.MainViewController()
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
