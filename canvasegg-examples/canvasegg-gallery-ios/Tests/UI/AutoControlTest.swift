46367776199ac87a561e61ef9c19ec4975380683
Hon-Atsugi-express <166384273+Hon-Atsugi-express@users.noreply.github.com>
2026-03-06 17:31:52 +0900

    Change

--- canvasegg-examples/canvasegg-gallery-ios/Tests/UI/AutoControlTest.swift
+++ canvasegg-examples/canvasegg-gallery-ios/Tests/UI/AutoControlTest.swift
@@ -10,8 +10,10 @@ class AutoControlTest: XCTestCase {
     func testViewAndScroll() {
         let app = XCUIApplication()
         app.launch()
+        Thread.sleep(forTimeInterval: 5.0)
         let normalized = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
         let coordinate = normalized.withOffset(CGVector(dx: 294, dy: 184))
         coordinate.tap()
+        Thread.sleep(forTimeInterval: 5.0)
     }
 }
