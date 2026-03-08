bd7ddca73c38905309a455b0c95cde8f9a443e70
Hon-Atsugi-express <166384273+Hon-Atsugi-express@users.noreply.github.com>
2026-03-08 16:34:46 +0900

    Change

--- canvasegg-examples/canvasegg-gallery-ios/Tests/UI/AutoControlTest.swift
+++ canvasegg-examples/canvasegg-gallery-ios/Tests/UI/AutoControlTest.swift
@@ -1,19 +1,30 @@
 import XCTest
 
 class AutoControlTest: XCTestCase {
+
+    let app = XCUIApplication()
+
     override func setUp() {
-
-    }
-    override func tearDown() {
-
-    }
-    func testViewAndScroll() {
-        let app = XCUIApplication()
+        super.setUp()
+        continueAfterFailure = false
         app.launch()
+
+        let _ = app.wait(for: .runningForeground, timeout: 300)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testViewAndScroll() {
+        XCTAssertTrue(app.exists, "Not application launched")
         Thread.sleep(forTimeInterval: 5.0)
         let normalized = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
         let coordinate = normalized.withOffset(CGVector(dx: 294, dy: 184))
+
+
         coordinate.tap()
+
         Thread.sleep(forTimeInterval: 5.0)
     }
 }
